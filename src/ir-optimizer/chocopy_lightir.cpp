4b3793b751eb9cc2fa1b41b4f1c0854bec9d9866
Letong Han <hanlt@shanghaitech.edu.cn>
2023-05-25 17:42:53 +0800

    [fix] (chocopy_lightir) flush output_stream

--- src/ir-optimizer/chocopy_lightir.cpp
+++ src/ir-optimizer/chocopy_lightir.cpp
@@ -1472,6 +1472,7 @@ int main(int argc, char *argv[]) {
 
     std::ofstream output_stream(target_path + ".ll");
     output_stream << IR;
+    output_stream.flush();
 
     if (emit) {
         cout << IR;
